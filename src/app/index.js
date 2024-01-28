b2eec14944acb843ab1735981c93c7362daeeb57
Anton1273751 <kronoss161@gmail.com>
2024-01-28 13:56:49 +0300

    work

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,23 @@
-const btnNode = document.querySelector(".form__btn");
+const crossNode = document.querySelector(".list__cross");
 const containerNode = document.querySelector(".container");
-console.log(btnNode);
+const delAllNode = document.querySelector(".click__delete-everything");
+const listsNode = document.querySelector(".lists");
+const listNode = document.querySelector(".list");
+const btnRemoveNode = document.querySelector(".click");
 
+// const element = document.querySelector('button')
 
+// element.addEventListener('click', function (event) {
+//   console.log('Произошло событие', event.type)
+// })
+
+delAllNode.addEventListener("click", () => {
+  //Удалает все
+
+  listsNode.remove("active");
+  btnRemoveNode.remove("active");
+});
+
+crossNode.addEventListener("click", (event) => {
+  listNode.remove("active");
+});
